ce4778670fd6320f5e676e2e2f2eedb3250d3b30
Jonathan Luu <72171222+jqluu@users.noreply.github.com>
2023-10-02 17:24:21 -0700

    login

--- website/auth.py
+++ website/auth.py
@@ -1,15 +1,23 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/login', methods=['Get', 'Post'])
 def login():
-    return "<p>login</>"
+    return render_template("login.html")
 
 @auth.route('/logout')
 def logout():
     return "<p>logout</p>"
 
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods=['Get', 'Post'])
 def sign_up():
-    return "<p>sign up</p>"
+    if request.method == 'POST':
+        email = request.form.get('email')
+        username = request.form.get('username')
+        password = request.form.get('password')
+
+        flash('Account created.', category='success')
+
+    return render_template("sign_up.html")
+
